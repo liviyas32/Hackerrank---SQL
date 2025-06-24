2d56317ef5ccab2535286765adfaf8a5b5bbfb2e
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2025-06-24 15:41:55 +0530

    Create SQL (Basic) Query Solutions.sql
    SQL Basic Query solutions are available here

---
+++ SQL (Basic) Query Solutions.sql
@@ -0,0 +1,49 @@
+/* SQL (Basic) Query solutions */
+
+-- #1	Revising the Select Query I -- 
+select *
+from city
+where (countrycode like '%USA%') and (population > 100000);
+
+
+-- #2 Revising the Select Query II --
+select *
+from city
+where (countrycode = 'USA') and (population > 120000);
+
+
+-- #3 Select All -- 
+select *
+from city;
+
+
+-- #4 Select By ID --
+select *
+from city
+where id = 1661;
+
+
+-- #5 Japanese Cities' Attributes --
+select *
+from city
+where countrycode = 'JPN';
+
+
+-- #6 Japanese Cities' Names --
+select name
+from city
+where countrycode = 'JPN';
+
+
+-- #7 Weather Observation Station 1 --
+select city, state
+from station;
+
+
+-- #8 Weather Observation Station 3 -- 
+select distinct city
+from station
+where mod(id,2) = 0;
+
+
+-- 
