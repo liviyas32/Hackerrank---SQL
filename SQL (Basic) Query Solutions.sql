07495bc48adaf1ec734df7c78f70410caa4ce4eb
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2025-06-27 00:12:23 +0530

    Update SQL (Basic) Query Solutions.sql

--- SQL (Basic) Query Solutions.sql
+++ SQL (Basic) Query Solutions.sql
@@ -235,18 +235,52 @@ where months*salary = (select max(months*salary) from employee)
 group by months*salary;
 
 
--- #28 
+-- #28 Weather Observation Station 2 --
+select round(sum(lat_n),2), round(sum(long_w),2)
+from station;
 
 
+-- #29 Weather Observation Station 13 --
+select truncate(sum(lat_n),4)
+from station
+where lat_n between 38.7880 and 137.2345;
 
 
+-- #30 Weather Observation Station 14 --
+select truncate(max(lat_n),4)
+from station
+where lat_n<137.2345;
 
 
+-- #31 Weather Observation Station 15 --
+select round(long_w,4)
+from station
+where lat_n = (select max(lat_n) 
+               from station 
+               where lat_n < 137.2345);
 
 
+-- #32 Weather Observation Station 16 --
+select round(min(lat_n),4)
+from station
+where lat_n in (select lat_n
+              from station
+              where lat_n > 38.7780);
+-- -------------------------------------
+select round(min(lat_n),4)
+from station
+where lat_n > 38.7780;
 
 
+-- #33 Weather Observation Station 17 --
+select round(long_w,4)
+from station
+where lat_n = (select min(lat_n)
+              from station
+              where lat_n>38.7780);
 
+
+-- #
 
 
 
