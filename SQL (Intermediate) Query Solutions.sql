5ed2f2c8fc5c05c48757b2969b67cfbada0ec636
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2025-07-09 12:39:42 +0530

    Update SQL (Intermediate) Query Solutions.sql

--- SQL (Intermediate) Query Solutions.sql
+++ SQL (Intermediate) Query Solutions.sql
@@ -14,3 +14,17 @@ union
 from temp
 where row_no = (select max(row_no) from temp));
 
+
+-- #2 New Companies --
+select c.company_code, c.founder, count(distinct lm.lead_manager_code),
+count(distinct sm.senior_manager_code), count(distinct m.manager_code),
+count(distinct e.employee_code)
+from company as c inner join lead_manager as lm on c.company_code = lm.company_code
+inner join senior_manager as sm on lm.company_code = sm.company_code
+inner join manager as m on sm.company_code = m.company_code
+inner join employee as e on m.company_code = e.company_code
+group by c.company_code, c.founder
+order by c.company_code asc;
+
+
+-- #
