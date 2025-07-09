2c2e4bac8035641bbf92f9615b50e1ee4a4639a8
liviyas32 <125739869+liviyas32@users.noreply.github.com>
2025-07-09 11:45:36 +0530

    Update SQL (Intermediate) Query Solutions.sql

--- SQL (Intermediate) Query Solutions.sql
+++ SQL (Intermediate) Query Solutions.sql
@@ -1,1 +1,16 @@
+/* SQL (Intermediate) Query Solutions */
+
+-- #1 Weather Observation Station 5 -- 
+with temp as (
+select city, length(city), row_number() over() as row_no
+from station
+order by length(city), city asc)
+
+(select city, length(city)
+from temp
+where row_no = (select min(row_no) from temp))
+union
+(select city, length(city)
+from temp
+where row_no = (select max(row_no) from temp));
 
